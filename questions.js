d96682d1ea34fd8bc47051a13184b3588733e061
Kindra Kinney <kindrakinney@gmail.com>
2020-01-06 22:42:58 -0800

    added final files

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is the correct way to call the random method on the Math global object?",
+        choices: ["Math.random()", "random.Math()", "Math(random)", "math.random()"],
+        answer: "Math.random()"
+    },
+    {
+        title: "Which HTML element do we put the Javascript?",
+        choices: ["<scripting>", "<javascript>", "<js>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        title: "What are variables used for in JavaScript Programs?",
+        choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks","None of the above"],
+        answer: "Storing numbers, dates or other valies"
+    }
+  ]
+  
